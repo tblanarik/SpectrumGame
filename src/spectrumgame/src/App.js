c3b9be3801c610acc347eb052b7d5b3cfb76d5f4
Trevor Blanarik <tblanarik@gmail.com>
2020-01-30 23:10:04 -0800

    adding speedo

--- src/spectrumgame/src/App.js
+++ src/spectrumgame/src/App.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ReactSpeedometer from "react-d3-speedometer"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Spectrum
-        </p>
-      </header>
+      <ReactSpeedometer/>
     </div>
   );
 }
