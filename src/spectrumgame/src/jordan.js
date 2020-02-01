64d7b2e9bb0e766f20437cb19af83f5580fe5034
Jordan Villegas <jordan@macbook-pro.lan>
2020-01-31 21:59:04 -0800

    wip

---
+++ src/spectrumgame/src/jordan.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import 'typeface-roboto';
+
+class HeaderTrevor extends React.Component {
+    render() {
+        return (
+        <div className="App">
+        <h1>testing fonts</h1>
+          <h2>this is an H2</h2>
+          <p>Paragraph text baby, yeah!"</p>
+          <p>more paragraphs</p>
+      </div>
+        )
+    }
+}
+
+export default HeaderTrevor;
